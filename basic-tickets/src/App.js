85a45695d8f4ea3e4d4b341c796704f731f35b50
DefinitelyAlec <alecparent@rocketmail.com>
2024-11-30 20:49:31 -0700

    install postcss and daisyui

--- basic-tickets/src/App.js
+++ basic-tickets/src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1> 
-      </header>
-    </div>
+    <button className='btn'>button</button>
+
   );
 }
 
