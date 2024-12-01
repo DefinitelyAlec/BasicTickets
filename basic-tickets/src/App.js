bcf2877674cb5698b12b2598d618bda43d66e83e
DefinitelyAlec <alecparent@rocketmail.com>
2024-11-30 21:03:28 -0700

    adding theme controller

--- basic-tickets/src/App.js
+++ basic-tickets/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { useEffect } from 'react';
+import {themeChange} from 'theme-change'
+
+
+
+function Theme() {
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
+}
 
 function App() {
   return (
-    <button className='btn'>button</button>
-
+    <>
+      <Theme/>
+      <button className='btn' data-set-theme='light'>light</button>
+      <button className='btn' data-set-theme='dark' >dark</button>
+    </>
   );
 }
 
